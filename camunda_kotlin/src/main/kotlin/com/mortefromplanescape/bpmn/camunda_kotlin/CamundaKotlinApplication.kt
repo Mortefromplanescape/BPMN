95c0430dcde0f36abe15f535de459690586db2fc
Mortefromplanescape <boreich89@gmail.com>
2022-08-24 15:56:28 +0300

    Сборка с базой

--- camunda_kotlin/src/main/kotlin/com/mortefromplanescape/bpmn/camunda_kotlin/CamundaKotlinApplication.kt
+++ camunda_kotlin/src/main/kotlin/com/mortefromplanescape/bpmn/camunda_kotlin/CamundaKotlinApplication.kt
@@ -1,8 +1,9 @@
 package com.mortefromplanescape.bpmn.camunda_kotlin
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
+@EnableProcessApplication
 @SpringBootApplication
 class CamundaKotlinApplication
 
